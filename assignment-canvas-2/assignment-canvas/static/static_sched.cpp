484410e85ddf7f219ec2f39c6cc3581f252f1b42
Wes <jwesgore@gmail.com>
2022-09-21 19:09:42 -0400

    first commit

---
+++ assignment-canvas-2/assignment-canvas/static/static_sched.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <chrono>
+#include <cmath>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+int main (int argc, char* argv[]) {
+
+  if (argc < 8) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <sync>"<<std::endl;
+    return -1;
+  }
+    
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+  // do your calculation here
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+
+  // report reult and time
+  std::cout<<result<<std::endl;
+std::cerr<<elapsed_seconds.count()<<std::endl;
+  
+  return 0;
+}
