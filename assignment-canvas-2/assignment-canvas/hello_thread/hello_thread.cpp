b4a993dc93e8f1e0542072695d9456e8dd448c76
jwesgore <jwesgore@gmail.com>
2022-09-22 15:13:15 -0400

    test

--- assignment-canvas-2/assignment-canvas/hello_thread/hello_thread.cpp
+++ assignment-canvas-2/assignment-canvas/hello_thread/hello_thread.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
+#include <pthread.h>
+
+// print method
+void *f(void *p) {
+
+  int pt;
+  pt = *(int *) p;
+
+  printf("I am thread %d in nbthreads \n", pt);
+  return NULL;
+}
 
 int main (int argc, char* argv[]) {
 
@@ -15,9 +24,20 @@ int main (int argc, char* argv[]) {
   int n;
   n = atoi(argv[1]);
 
-  printf("I am thread %d in nbthreads", n);
+  // initialize variables
+  pthread_t threads[n];
+  long taskids[n];
 
+  //create threads
+  for (int i = 0; i < n; ++i) {
+    taskids[i] = i;
+    pthread_create(&threads[i], NULL, f, (void*)&taskids[i]);
+  }
+
+  // join threads
+  for (int i = 0; i < n; ++i) {
+    pthread_join(threads[i], NULL);
+  }
 
 	return 0;
-
 }
