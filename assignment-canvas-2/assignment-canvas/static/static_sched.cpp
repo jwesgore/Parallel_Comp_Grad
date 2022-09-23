6fcb776b2a337f01611d6b9ebd4030a1417d22d9
jwesgore <jwesgore@gmail.com>
2022-09-22 20:16:01 -0400

    test

--- assignment-canvas-2/assignment-canvas/static/static_sched.cpp
+++ assignment-canvas-2/assignment-canvas/static/static_sched.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <chrono>
 #include <cmath>
+#include <pthread.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,6 +19,62 @@ float f4(float x, int intensity);
 }
 #endif
 
+// method to parse function 
+typedef float (*ptr) (float,int);
+ptr getFunction(int f) {
+  switch (f) {
+    case 1:
+      return &f1;
+    case 2:
+      return &f2;
+    case 3:
+      return &f3;
+    default:
+      return &f4;
+  }
+}
+
+pthread_mutex_t mu;
+
+// structure
+  struct mStruct{
+    int a;
+    float co;
+    ptr func;
+    int intensity;
+    float* result;
+    int start;
+    int end;
+  };
+
+void* thread(void* p) {
+  mStruct temp;
+  temp = *(mStruct *) p;
+  float y = 0;
+
+  for (int i = temp.start; i < temp.end; i++) {
+    float x = (*temp.func)(temp.a + ((i + .5) * temp.co), temp.intensity);
+    y += x;
+  }
+
+  pthread_mutex_lock(&mu);
+  *temp.result += y * temp.co;
+  pthread_mutex_unlock(&mu);
+  return 0;
+}
+
+void* iter(void* p) {
+  mStruct temp;
+  temp = *(mStruct *) p;
+
+  for (int i = temp.start; i < temp.end; i++) {
+    float x = (*temp.func)(temp.a + ((i + .5) * temp.co), temp.intensity);
+    pthread_mutex_lock(&mu);
+    *temp.result += x * temp.co;
+    pthread_mutex_unlock(&mu);
+  }
+  return 0;
+}
 
 int main (int argc, char* argv[]) {
 
@@ -30,12 +87,59 @@ int main (int argc, char* argv[]) {
 
   // do your calculation here
 
+  // convert command line values
+  int fid  =  atoi(argv[1]);
+  int a    =  atoi(argv[2]);
+  int b    =  atoi(argv[3]);
+  int n    =  atoi(argv[4]);
+  int intensity =  atoi(argv[5]);
+  int nbthreads =  atoi(argv[6]);
+  std::string sync       =  argv[7];
+  const std::string type = "thread";
+
+  // other variables
+  float (*ptr)(float, int) = getFunction(fid); // get function
+  float co =  (b - a) / float (n); // calculate coefficient
+  float result;
+  pthread_t threads[nbthreads];
+  mStruct threadData[nbthreads];
+  mStruct threadDataTemplate = {a, co, ptr, intensity, &result,0,1};
+
+  int spread = n / nbthreads;
+  int remainder   = n % nbthreads;
+
+  
+  //create threads
+  int point = 0;
+  for (int i = 0; i < nbthreads; ++i) {
+    
+    threadData[i] = threadDataTemplate;
+    threadData[i].start = point;
+
+    if (remainder > 0) {
+      remainder--;
+      point++;
+    }
+    
+    point += spread;
+    threadData[i].end = point;
+    if (sync.compare(type) == 0) 
+      pthread_create(&threads[i], NULL, thread, (void*)&threadData[i]);
+    else
+      pthread_create(&threads[i], NULL, iter, (void*)&threadData[i]);
+  }
+
+  // join threads
+  for (int i = 0; i < nbthreads; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+  
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
 
   // report reult and time
   std::cout<<result<<std::endl;
-std::cerr<<elapsed_seconds.count()<<std::endl;
+  std::cerr<<elapsed_seconds.count()<<std::endl;
   
   return 0;
 }
