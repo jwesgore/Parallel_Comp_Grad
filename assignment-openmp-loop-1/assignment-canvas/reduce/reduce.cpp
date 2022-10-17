e404bdee172b2d6b9d401005d4200551152b1ee4
jwesgore <jwesgore@gmail.com>
2022-10-17 19:20:09 -0400

    test

--- assignment-openmp-loop-1/assignment-canvas/reduce/reduce.cpp
+++ assignment-openmp-loop-1/assignment-canvas/reduce/reduce.cpp
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-struct timeval start, end;
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,7 +20,9 @@ extern "C" {
 
 int main (int argc, char* argv[]) {
     //forces openmp to create the threads beforehand
-#pragma omp parallel
+
+    long sum = 0;
+    #pragma omp parallel
     {
         int fd = open (argv[0], O_RDONLY);
         if (fd != -1) {
@@ -41,26 +43,30 @@ int main (int argc, char* argv[]) {
     int threads = atoi(argv[2]);
     char *scheduling = argv[3];
     int granularity = atoi(argv[4]);
-    int sum = 0;
+    struct timeval start, end;
+    int sumLocal = 0;
 
     generateReduceData (arr, atoi(argv[1]));
     
     gettimeofday(&start, NULL);
+    omp_set_num_threads(threads);
 
     // insert reduction code here
-    if (granularity > 0) {
+        granularity <= 0 ? granularity = 1 : NULL;
+
         if (scheduling == "dynamic") {
-            #pragma omp parallel for shared(sum) schedule(dynamic, granularity) num_threads(threads)
+            #pragma omp parallel for schedule(dynamic, granularity) reduction(+:sum)
             for (int i = 0; i < n; i++) {
                 sum += arr[i];
             }
         } else {
-            #pragma omp parallel for shared(sum) schedule(static, granularity) num_threads(threads)
-            for (int i = 0; i < n; i++) {
+            #pragma omp parallel for schedule(static, granularity) reduction(+:sum)
+            for (int i = 0; i < n; i++)
                 sum += arr[i];
-            }
+        
         }
-    }
+    
+    
     
     gettimeofday(&end, NULL);
     double last_time = (end.tv_sec-start.tv_sec + (end.tv_usec - start.tv_usec)/1000000);
