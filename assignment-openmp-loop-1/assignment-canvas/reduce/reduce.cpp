a8049a702da8b2ae67e6e74844c51995e8663727
jwesgore <jwesgore@gmail.com>
2022-10-17 18:42:44 -0400

    test 1

--- assignment-openmp-loop-1/assignment-canvas/reduce/reduce.cpp
+++ assignment-openmp-loop-1/assignment-canvas/reduce/reduce.cpp
@@ -3,9 +3,11 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+struct timeval start, end;
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,12 +38,35 @@ int main (int argc, char* argv[]) {
     
     int n = atoi(argv[1]);
     int * arr = new int [n];
-    
+    int threads = atoi(argv[2]);
+    char *scheduling = argv[3];
+    int granularity = atoi(argv[4]);
+    int sum = 0;
+
     generateReduceData (arr, atoi(argv[1]));
     
-    // insert reduction code here
+    gettimeofday(&start, NULL);
 
+    // insert reduction code here
+    if (granularity <= 0) granularity = 1;
+
+    if (scheduling == "dynamic") {
+        #pragma omp parallel for schedule(dynamic, granularity) num_threads(threads)
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+    } else {
+        #pragma omp parallel for schedule(static, granularity) num_threads(threads)
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+    }
     
+    gettimeofday(&end, NULL);
+    double last_time = (end.tv_sec-start.tv_sec + (end.tv_usec - start.tv_usec)/1000000);
+    std::cout << sum << std::endl;
+    std::cerr << last_time << std::endl;
+
     delete[] arr;
     
     return 0;
