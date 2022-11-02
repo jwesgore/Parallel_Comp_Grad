2eac387bffe3243f39361f36a4ab60b17dbfd88a
jwesgore <jwesgore@gmail.com>
2022-11-01 22:56:08 -0400

    supid test

--- assignment-openmp-2-canvas/assignment-openmp-2-canvas/mergesort-task/mergesort-task.cpp
+++ assignment-openmp-2-canvas/assignment-openmp-2-canvas/mergesort-task/mergesort-task.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <omp.h>
 #include <string.h>
+#include <vector>
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,6 +20,58 @@ extern "C" {
 }
 #endif
 
+void merge(int* arr, int left, int right, int mid) {
+  
+  std::vector <int> leftVector, rightVector;
+  int lposition = 0, rposition = 0 , tposition = left;
+
+  for (int i = left; i <= mid; i++) {
+    leftVector.push_back(arr[i]);
+  }
+  for (int i = mid + 1; i <= right; i++) {
+    rightVector.push_back(arr[i]);
+  }
+
+  while (lposition < leftVector.size() && rposition < rightVector.size()) {
+    
+    if (leftVector[lposition] < rightVector[rposition]) {
+      arr[tposition] = leftVector[lposition];
+      lposition++;
+    } else {
+      arr[tposition] = rightVector[rposition];
+      rposition++;
+    }
+    tposition++;
+  }
+
+  while(lposition < leftVector.size()) {
+    arr[tposition] = leftVector[lposition];
+    lposition++, tposition++;
+  }
+
+  while(rposition < rightVector.size()) {
+    arr[tposition] = rightVector[rposition];
+    rposition++, tposition++;
+  }
+
+}
+
+void mergeSort(int* arr, int left, int right) {
+  
+  int mid = (left + right) / 2;
+  
+  if (left >= right) return;
+
+  #pragma omp task shared(arr)
+  mergeSort(arr, left, mid);
+
+  #pragma omp task shared(arr)
+  mergeSort(arr, mid + 1, right);
+
+  #pragma omp taskwait
+  merge(arr, left, right, mid);
+
+}
 
 int main (int argc, char* argv[]) {
 
@@ -51,10 +104,19 @@ int main (int argc, char* argv[]) {
   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
   //insert sorting code here.
 
+  omp_set_num_threads(numThreads);
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      mergeSort(arr, 0, n-1);
+    }
+  }
+
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapased_seconds = end-start;
   //Print the total execution time (in sec) to the error stream
-  cerr<<elapased_seconds.count()<<std::endl;
+  std::cerr<<elapased_seconds.count()<<std::endl;
   //printArray(arr,n);
   checkMergeSortResult (arr, n);
 
