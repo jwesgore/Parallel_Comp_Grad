98a5230a3657ecb523eb8948226e735c87300970
jwesgore <jwesgore@gmail.com>
2022-11-18 01:54:16 -0500

    fjfj'

--- 7. assignment-mpi-basic-1/7. assignment-mpi-basic/num_int/test.sh
+++ 7. assignment-mpi-basic-1/7. assignment-mpi-basic/num_int/test.sh
@@ -2,11 +2,10 @@
 
 # does not account for neg numbers
 sci_notation_regex='^[0-9]+([.][0-9]+)?(e[0-9]+|e-[0-9]+)?$'
-slurm='^srun:'
 
 function test_time {
     # compare 
-    if [[ ! $1 =~ $sci_notation_regex ]] || [[ ! $1 =~ $slurm ] && [ ! $3 =~ $sci_notation_regex ]] ; 
+    if [[ ! $1 =~ $sci_notation_regex ]] || [[ ! $3 =~ $sci_notation_regex ]] ; 
     then
         echo ERROR: time is not on stderr or not formatted properly
         echo
