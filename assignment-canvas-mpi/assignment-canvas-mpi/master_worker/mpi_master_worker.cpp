bd467d68e1dfd4d5686214a55cc7bb69699d4231
jwesgore <jwesgore@gmail.com>
2022-12-01 18:51:53 -0500

    test 1.7

--- assignment-canvas-mpi/assignment-canvas-mpi/master_worker/mpi_master_worker.cpp
+++ assignment-canvas-mpi/assignment-canvas-mpi/master_worker/mpi_master_worker.cpp
@@ -98,7 +98,7 @@ void worker(int fid, float co, int a, int intensity, int n){
 
       rank_val *= co;
 
-      MPI_Send(rank_val, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD); // send back results
+      MPI_Send(&rank_val, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD); // send back results
     }
   }
 }
