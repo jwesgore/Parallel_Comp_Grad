5bc044ce82946b1fd8346b4f1cb50847239f0525
jwesgore <jwesgore@gmail.com>
2022-12-06 14:07:35 -0500

    hostname

--- assignment-01/assignment-01/prelim.c
+++ assignment-01/assignment-01/prelim.c
@@ -2,10 +2,13 @@
 #include <limits.h>
 #include <stdio.h>
 
+#ifndef HOST_NAME_MAX
+#define HOST_NAME_MAX 255
+#endif
+
 int main () {
-	
-  char hostname[HOST_NAME_MAX + 1];
-	gethostname(hostname, HOST_NAME_MAX + 1);
+  char hostname[HOST_NAME_MAX];
+	gethostname(hostname, HOST_NAME_MAX);
 
   printf("Hostname: %s\n", hostname);
   return 0;
